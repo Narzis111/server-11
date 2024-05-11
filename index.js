343731a9479d6f0fcb85baff6606e32559f1bb18
Narzis111 <narzisnahar111@gmail.com>
2024-05-11 06:16:08 +0100

    collection created and data get code created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,113 +26,110 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        // const itemCollection = client.db('craftDB').collection('item');
-        // const categoryCollection = client.db('craftDB').collection('category');
-
+        const assignmentCollection = client.db('assignmentDB').collection('assign');
+        const submitCollection = client.db('assignmentDB').collection('submit');
         // =====================================================
 
         // ok oldone
 
         // // to find all (read)
-        // app.get('/item', async (req, res) => {
-        //     const cursor = itemCollection.find();
-        //     const result = await cursor.toArray();
-        //     res.send(result);
-        // })
+        app.get('/assignment', async (req, res) => {
+            const cursor = assignmentCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
         // // update korbo specific 1ta id ke tai find single params lagbe new lagbe
         // // to find single (read)
-        // app.get('/item/:id', async (req, res) => {
+        app.get('/assignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await assignmentCollection.findOne(query);
+            res.send(result);
+        })
+
+        //   create data
+        app.post('/assignment', async (req, res) => {
+            const newAssign = req.body;
+            console.log(newAssign);
+            const result = await assignmentCollection.insertOne(newAssign);
+            res.send(result);
+        })
+        // update specific id 
+
+        app.put('/assignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const update = req.body;
+
+            const assign = {
+
+                $set: {
+                    thumbnail_url: update.thumbnail_url,
+                    title: update.title,
+                    marks: update.marks,
+                    description: update.description,
+                    difficulty_level: update.difficulty_level,
+                    due_date: update.due_date
+                   
+                }
+            }
+
+            const result = await assignmentCollection.updateOne(filter, assign, options);
+            res.send(result);
+        })
+
+        // delete korbo specific 1ta id ke tai find single params lagbe new lagbe
+
+        app.delete('/assignment/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await assignmentCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // submit assignment
+
+        
+        // app.delete('/submit/:id', async (req, res) => {
         //     const id = req.params.id;
         //     const query = { _id: new ObjectId(id) }
-        //     const result = await categoryCollection.findOne(query) || await itemCollection.findOne(query);
+        //     const result = await submitCollection.deleteOne(query);
         //     res.send(result);
         // })
-
-
-        // // to find category (read)
-        // app.get('/category', async (req, res) => {
-        //     const cursor = categoryCollection.find();
+        // app.get('/submit', async (req, res) => {
+        //     const cursor = submitCollection.find();
         //     const result = await cursor.toArray();
         //     res.send(result);
         // })
-        // // to find single  (read)
-        // app.get('/category/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await categoryCollection.findOne(query);
-        //     res.send(result);
-        // })
-        // app.get("/category/:catID", async (req, res) => {
-        //     console.log(req.params.catID);
-        //     const result = await categoryCollection.find({ catID: req.params.catID }).toArray();
-        //     res.send(result)
-        // })
-        // // deatil
-        // app.get('/detailcat/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const clickedCategory = await categoryCollection.findOne({ _id: new ObjectId(id) });
-        //     const subcategory = clickedCategory.subcategory;
-        //     const categoryData = await categoryCollection.find({ subcategory }).toArray();
-        //     const itemsData = await itemCollection.find({ subcategory }).toArray();
-
-        //     const combinedData = {
-        //         category: categoryData,
-        //         item: itemsData
-        //     };
-
-        //     res.json(combinedData);
-        // })
-
-        // // to find single user (read)
-        // app.get("/myItems/:email", async (req, res) => {
+           // to find single user (read)
+        // app.get("/mySubmit/:email", async (req, res) => {
         //     console.log(req.params.email);
-        //     const result = await itemCollection.find({ email: req.params.email }).toArray();
+        //     const result = await submitCollection.find({ email: req.params.email }).toArray();
         //     res.send(result)
         // })
 
-        // //   create data
-        // app.post('/item', async (req, res) => {
-        //     const newItem = req.body;
-        //     console.log(newItem);
-        //     const result = await itemCollection.insertOne(newItem);
-        //     res.send(result);
-        // })
-        // // update specific id 
+        app.get('/submit', async (req, res) => {
+            console.log(req.query.email);
+            // console.log('tok tok token', req.cookies.token);
+            // console.log('user in the valid token', req.user);
+            // if(req.query.email !== req.user.email){
+            //     return res.status(403).send({message: 'forbidden access'})
+            // }
+            let query = {}
+            if(req.query?.email){
+                query = {email:req.query.email}
+            }
+            const result = await submitCollection.find(query).toArray();
+            res.send(result);
+        })
 
-        // app.put('/item/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: new ObjectId(id) }
-        //     const options = { upsert: true };
-        //     const updateItem = req.body;
-
-        //     const item = {
-
-        //         $set: {
-        //             image: updateItem.image,
-        //             item_name: updateItem.item_name,
-        //             subcategory: updateItem.subcategory,
-        //             shortDescription: updateItem.shortDescription,
-        //             price: updateItem.price,
-        //             rating: updateItem.rating,
-        //             customization: updateItem.customization,
-        //             processing_time: updateItem.processing_time,
-        //             stockStatus: updateItem.stockStatus,
-
-        //         }
-        //     }
-
-        //     const result = await itemCollection.updateOne(filter, item, options);
-        //     res.send(result);
-        // })
-
-        // // delete korbo specific 1ta id ke tai find single params lagbe new lagbe
-
-        // app.delete('/item/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await itemCollection.deleteOne(query);
-        //     res.send(result);
-        // })
+        app.post('/submit', async (req, res) => {
+            const submitted = req.body;
+            console.log(submitted);
+            const result = await submitCollection.insertOne(submitted);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
